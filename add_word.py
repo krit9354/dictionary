1d6b61d689d40c550434a68cdb9a407a7e923c07
krit9354 <krit9354@gmail.com>
2020-09-20 12:13:42 +0700

    Initial commit

---
+++ add_word.py
@@ -0,0 +1,20 @@
+import csv
+import tkinter
+
+def addWord():
+    add = word.get()
+    Fmean = mean.get()
+    with open('word.csv', mode='a',newline="",encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow([add,Fmean])
+
+
+window = tkinter.Tk()
+text1 = tkinter.Label(window, text="word to add : ").grid(row=0,column=0)
+text2 = tkinter.Label(window, text="mean of word : ").grid(row=1,column=0)
+button=tkinter.Button(window,text="click",command=addWord).grid(row=3,column=1)
+word = tkinter.Entry(window,width=10)
+mean = tkinter.Entry(window,width=10)
+word.grid(row=0,column=1)
+mean.grid(row=1,column=1)
+window.mainloop()
---
+++ search_word.py
@@ -0,0 +1,26 @@
+import csv
+import tkinter
+dict = {}
+
+
+with open("word.csv",mode="r",encoding='utf-8')as cvs_file:
+    reader = csv.reader(cvs_file, delimiter=',')
+    for row in reader:
+        if row != []:
+            dict[row[0]] = row[1]
+        else:
+            pass
+
+def searchWord():
+    sellect = user.get()
+    if sellect in dict.keys():
+        print("mean :", dict[sellect])
+        text = tkinter.Label(window, text=" : "+dict[sellect]).grid(row=0,column=1)
+    else:
+        print("errror!.try again")
+
+window = tkinter.Tk()
+button=tkinter.Button(window,text="click",command=searchWord).grid(row=1)
+user = tkinter.Entry(window,width=10)
+user.grid(row=0,column=0)
+window.mainloop()
